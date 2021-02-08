07231e73fec4067b39605656460dcf0c4db3023c
AnttiHaerkoenen <antth@hotmail.fi>
2021-02-08 16:21:19 +0200

    douple boxplot

--- src/visualization/plot_distances.py
+++ src/visualization/plot_distances.py
@@ -4,15 +4,21 @@ import pandas as pd
 import numpy as np
 import geopandas as gpd
 from shapely.geometry import Point, LineString
+import seaborn as sns
 import matplotlib.pyplot as plt
 
+sns.set_theme(style="ticks", palette="pastel")
 
-color_mapper = {
-    1.0: "grey",
-    2.0: "green",
-    3.0: "blue",
-    4.0: "red",
-}
+
+def distances(data, locations):
+    distance_data = []
+    for k, v in locations.items():
+        d = data.copy()
+        d['landmark'] = k
+        d['distance'] = d.geometry.centroid.distance(v)
+        distance_data.append(d)
+    data = pd.concat(distance_data)
+    return data
 
 
 if __name__ == '__main__':
@@ -23,23 +29,22 @@ if __name__ == '__main__':
 
     plots = gpd.read_file(data_dir / 'plots.shp', index_col=0).set_index('number', drop=True)
     plots = plots.to_crs(epsg=3067)
-    plots['distance'] = plots.geometry.centroid.distance(square)
-    plots = pd.DataFrame(plots)
     plots.quartile.fillna('NA', inplace=True)
     plots['quartile'] = plots.quartile.apply(lambda c: int(c) if not isinstance(c, str) else c)
     gentry = plots.status == 'g'
     plots.loc[gentry, 'quartile'] = 'gentry'
-    # plots.plot(
-    #     x='distance',
-    #     y='tax',
-    #     kind='scatter',
-    #     c='quartile',
-    #     colormap=plt.get_cmap('Set2', 4),
-    #     colorbar=False,
-    # )
-    plots.boxplot(
-        column='distance',
-        by='quartile',
+    plots = distances(plots, {'Market square': square, 'Church street': church_street})
+    order = ['gentry', 1, 2, 3, 4, 'NA']
+    plt.rc('legend', loc='upper center')
+    plot_1 = sns.boxplot(
+        y='distance',
+        x='quartile',
+        hue='landmark',
+        data=plots,
+        order=order,
+        palette=["m", "g"],
     )
-    plt.savefig(fig_dir / 'dist_plot.png')
+    plot_1.get_legend().set_title('Distance to')
     plt.show()
+    fig = plot_1.get_figure()
+    fig.savefig(fig_dir / 'dist_plot.png')
